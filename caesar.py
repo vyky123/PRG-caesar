86b815b81da59a1ff844a071e147e63cc3025f30
vyky <jinajik@seznam.cz>
2018-10-08 17:45:06 +0200

    z domova 8.10.

--- caesar.py
+++ caesar.py
@@ -1,42 +1,68 @@
 #!/usr/bin/env python3
-# Soubor:  caesar.py
-# Datum:   02.10.2018 00:36
-# Autor:   Marek Nožka, nozka <@t> spseol <d.t> cz
-############################################################################
-import unicodedata
-from sys import argv, stdin, stdout, stderr
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  7 12:52:58 2017
 
+@author: vyk35227
+"""
+vyber = input("Posun v abecedě nebo klíčové slovo? POSUN/KLIC > ").upper() 
+    
+if vyber == "POSUN":            #posun v abecedě
+    n = int(input("Zadej číslo posunu > "))
+    m = input("Zadej text > ").upper()
+    a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    b = a[n:] + a[:n]
+    f = 0
+    
 
-PISMENA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    if " " in m:
+        m = m.replace(" ","")
+        
+    for i in m:
+        g = m[f]
+        f = f + 1
+        x = b[a.index(g)]
+        print(x, end="")
 
-try:
-    KLIC = int(argv[1])
-except IndexError:
-    stderr.write(argv[0])
-    stderr.write(': CHYBA: Jak parametr musíš zadat klíč!\n\n')
-    exit(2)
-except ValueError:
-    stderr.write(argv[0])
-    stderr.write(': CHYBA: Klíč je celé číslo!.\n\n')
-    exit(3)
-
-if stdin.isatty():
-    print('Zadej text a ukonči ho Ctrl+D. (Ve Widlích Ctrl+Z)')
-text = stdin.read()
-
-text = ''.join(text.split())
-text = unicodedata.normalize('NFD', text).upper()\
-        .encode('ascii', 'ignore').decode('ascii')
-
-if len(argv) > 2:
-    print()
-    print(text)
-    print()
-
-for c in text:
-    try:
-        i = PISMENA.index(c)
-    except ValueError:
-        continue
-    stdout.write(PISMENA[(i + KLIC) % len(PISMENA)])
-print()
+else:
+    if vyber == "KLIC":                         #pomocí klíčového slova
+        m = input("Zadej text > ").upper()
+        a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        f = 0       
+        x = ""
+        sif = input("Šifrovat nebo dešifrovat? Zadej SIF/DES > ").upper()  
+        
+           
+        if sif == "SIF":                        #šifrování
+            n = input("Zadej klíč > ").upper() 
+            
+            for i in m:
+                cislo = a.find(i)
+                f = 0
+                cislo = cislo + ( (ord(n[f])) - ord("A") )
+                f = f + 1
+                f = f % len(n)
+                if cislo >= len(a):
+                    cislo = cislo - len(a)
+                        
+                x = x + a[cislo]
+        
+        else:
+            if sif == "DES":                    #dešifrování
+                n = input("Zadej klíč > ").upper() 
+                for i in m:
+                    cislo = a.find(i)
+                    f = 0
+                    cislo = cislo - ((ord(n[f])) - ord("A") )
+                    f = f + 1
+                    f = f % len(n)
+                    if cislo >= len(a):
+                        cislo = cislo - len(a)
+                            
+                    x = x + a[cislo]
+            else:
+                print("Odpověď musí být SIF/DES")
+        print(x)
+    else:
+        print("Odpověď musí být POSUN/KLIC")
+            
